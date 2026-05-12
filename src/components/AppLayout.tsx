ac19d1468d884400eb473a68e56ffde55dbc4956
harshithareddy603 <harshitareddy919@gmail.com>
2026-05-12 09:50:40 +0530

    fix: add missing Platform import in AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, ScrollView, Image, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, ScrollView, Image, SafeAreaView, Platform } from 'react-native'
 import React, { ReactNode, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useDocuments } from "../hooks/useDocuments";
